b6278f180cba622aac6f02f98273e60a054fdd3c
Nikolay Ganev <nikolay.ganev@symphony.foundation>
2018-10-26 19:01:49 +0200

    adding logout

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,6 +5,13 @@ firebase.auth().onAuthStateChanged(function(user) {
 
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null) {
+    	var email_id = user.email;
+    	document.getElementById("user_para").innerHTML = "Welcome User : " + email;
+    }
   } else {
     // No user is signed in.
     document.getElementById("user_div").style.display = "none";
@@ -12,14 +19,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
 
-  window.alert("Error : " + errorMessage);
-  // ...
-});
 
 
 function login () {
@@ -27,5 +27,16 @@ function login () {
 	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
 
-	window.alert(userEmail + " " + userPass);
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+
+	  window.alert("Error : " + errorMessage);
+	  // ...
+	});
+	
+}
+function logout(){
+	firebase.auth().signOut();
 }
